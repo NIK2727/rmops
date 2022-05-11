c2ab32395a05437164f27cd3317b88739bc35281
NIK2727 <nikhil@robin.io>
2022-05-11 07:29:08 +0000

    Update mypython1

--- python1/main.py
+++ python1/main.py
@@ -1,1 +1,10 @@
-print("hello")
+#!/usr/bin/python3
+import os
+import json
+print("Alright")
+f = os.environ.get('MDCAP_ELEMENT_CONF_PATH')
+print(f)
+with open(f,'r') as fh:
+    data = fh.read()
+    data1 = json.loads(data)
+    print(data1)
